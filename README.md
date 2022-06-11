09c436c40b384d3f52aecfda6ed6fee441f82a34
jackhclee <38123458+jackhclee@users.noreply.github.com>
2022-06-12 00:38:15 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cyf-hotels-api
 Example CYF Hotels API
+
+## Prerequisities
+1. [NodeJS 16.15.1](https://nodejs.org/en/)
+2. [Nodemon](https://www.npmjs.com/package//nodemon) 
+3. Visual Studio Code
+4. Visual Studio Code Extension - [SQLTools](https://vscode-sqltools.mteixeira.dev)
+5. Visual Studio Code Extension - [SQLTools PostgreSQL/Redshift Driver](https://marketplace.visualstudio.com/items?itemName=mtxr.sqltools-driver-pg)
+6. Visual Studio Code Extension - [SQL Formatter](https://marketplace.visualstudio.com/publishers/adpyke)
+7. [PostgreSQL.app](https://postgresapp.com/)
+
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+app.use(express.json());
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_hotels",
+  password: "postgrespw",
+  port: 5432,
+});
+
+// app.get("/hotels", function (req, res) {
+//   pool
+//     .query("SELECT * FROM hotels")
+//     .then((result) => res.json(result.rows))
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json(error);
+//     });
+// });
+
+app.get("/hotels", function (req, res) {
+  const hotelNameQuery = req.query.name;
+  console.log(hotelNameQuery);
+  // let query = `SELECT * FROM hotels ORDER BY name`;
+  let query = "SELECT * FROM hotels " + " WHERE name like " + `'%${hotelNameQuery}%'`+ " ORDER BY name";
+  console.log(query);
+  let params = [];
+  if (hotelNameQuery) {
+      // query = `SELECT * FROM hotels WHERE name LIKE $1 ORDER BY name`;
+      // params.push(`%${hotelNameQuery}%`);
+  }
+  
+  pool
+    .query(query, params)
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.post("/hotels", function (req, res) {
+  const newHotelName = req.body.name;
+  const newHotelRooms = req.body.rooms;
+  const newHotelPostcode = req.body.postcode;
+
+  if (!Number.isInteger(newHotelRooms) || newHotelRooms <= 0) {
+    return res
+      .status(400)
+      .send("The number of rooms should be a positive integer.");
+  }
+
+  pool
+    .query("SELECT * FROM hotels WHERE name=$1", [newHotelName])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send("An hotel with the same name already exists!");
+      } else {
+        const query =
+          "INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3)";
+        pool
+          .query(query, [newHotelName, newHotelRooms, newHotelPostcode])
+          .then(() => res.send("Hotel created!"))
+          .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+          });
+      }
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
